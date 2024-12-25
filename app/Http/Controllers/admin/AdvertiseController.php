7591404aacb7fc44ec434bb43e0ba0e7564f63ec
silasrai <silasraii144@gmail.com>
2024-12-25 21:47:39 +0545

    Company and advertise controller

--- app/Http/Controllers/admin/AdvertiseController.php
+++ app/Http/Controllers/admin/AdvertiseController.php
@@ -12,7 +12,7 @@ class AdvertiseController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.advertise.index');
     }
 
     /**
@@ -20,7 +20,7 @@ class AdvertiseController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.advertise.create');
     }
 
     /**
@@ -44,7 +44,7 @@ class AdvertiseController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return view('admin.advertise.edit');
     }
 
     /**
