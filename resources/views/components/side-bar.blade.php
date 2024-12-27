29a5d581fbe75bbee2d5cce97cac788d2d2c4353
silasrai <silasraii144@gmail.com>
2024-12-27 13:48:21 +0545

    dashboard

---
+++ resources/views/components/side-bar.blade.php
@@ -0,0 +1,17 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="index.html"> <img alt="image" src="/assets/img/logo.png" class="header-logo" /> <span
+                class="logo-name">Otika</span>
+        </a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+        </li>
+        <li>
+            <a href="{{ route('company.index') }}" ><i
+                    data-feather="briefcase"></i><span>Company</span></a>
+        </li>
+    </ul>
+</aside>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
-<x-app-layout>
+ <x-app-layout>
+     {{--
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
@@ -13,5 +14,31 @@
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </div>  --}}
+     <section class="section">
+         <div class="row ">
+             <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                 <div class="card">
+                     <div class="card-statistic-4">
+                         <div class="align-items-center justify-content-between">
+                             <div class="row ">
+                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                     <div class="card-content">
+                                         <h5 class="font-15">New Booking</h5>
+                                         <h2 class="mb-3 font-18">258</h2>
+                                         <p class="mb-0"><span class="col-green">10%</span> Increase</p>
+                                     </div>
+                                 </div>
+                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                     <div class="banner-img">
+                                         <img src="assets/img/banner/1.png" alt="">
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
+ </x-app-layout>
